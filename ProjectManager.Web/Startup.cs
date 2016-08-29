7d2efc0f2cabacaacc4d90a2a2ed4d871ee25884
Ayodele <awoleyeayodele1@gmail.com>
2016-08-29 04:20:51 -0400

    Initial Project Setup

---
+++ ProjectManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManager.Web.Startup))]
+namespace ProjectManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
