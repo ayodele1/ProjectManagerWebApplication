7ddf6a3ad333e1d11905be695e674ebedd1cc87e
Ayodele <awoleyeayodele1@gmail.com>
2016-09-03 10:02:13 -0400

    Adde new Areas

---
+++ DataAccess/UserResponsibilityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DomainObjects
+{
+    public class UserResponsibilityRepository
+    {
+        private ProjectManagerContext context;
+
+        public ICollection<UserResponsibility> GetAll()
+        {
+            using (context = new ProjectManagerContext())
+            {
+                return context.UserResponsibilites.AsNoTracking().ToList();
+            }
+        }
+    }
+}
